0c3f773129f39e55c100b5b510e9879e35d7e57a
Ohmygodvt95 <ohmygodvt95@gmail.com>
2017-06-08 14:18:11 +0700

    Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # pages_controller
+  root "pages#show", act: "home"
+  get "/pages/:act" => "pages#show"
 end
