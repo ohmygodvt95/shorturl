19cb956bdab378600bfd22e0b9749a9e2b2890a0
Le Vinh Thien <le.vinh.thien@sun-asterisk.com>
2021-10-21 22:04:50 +0900

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,15 +6,4 @@ module ApplicationHelper
   def encode num
     num += INIT
     str = ""
-    while num > 0 do
-      str = ALPHABET[num % BASE] + str
-      num /= BASE
-    end
-    str.reverse
-  end
-
-  def full_title page_title = ""
-    base_title = t "base_title"
-    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
-  end
-end
+ 
